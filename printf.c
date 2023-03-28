6710b6528b07b22be4f7a7d02f50efc405efd92f
Tandanyi <tattyndanyi@gmail.com>
2023-03-28 06:49:35 +0000

    printf 0 without s

--- printf.c
+++ printf.c
@@ -8,41 +8,45 @@
 
 int _printf(const char *format, ...)
 {
-	int i;
-	char *s; /*for adding characters after replacing with an argument*/
-
-	if (format == NULL)
-		return (-1);
 	va_list arg1;
+
+	int i;
+	char *s = NULL;
+	char arg; /*for adding characters after replacing with an argument*/
+
+	arg = va_arg(arg1, int);
+	if (*format == '\0')
+		return (-1);
 
 	va_start(arg1,format); /*checks for c*/
 
-		for (i = 0; format[i] != NULL; i++)
+		for (i = 0; format[i] != '\0'; i++)
 		{
 			if (format[i] == '%' && format[i+1] == ' ')
 			{
 				continue;
 			}
 			else if (format[i] == '%' && format[i+1] == 'c')
-			{	char *newstring = malloc(sizeof(format) + sizeof(arg1) -2);
+			{	char *newstring = malloc(sizeof(format) + sizeof(arg1) -2);/*allocate size where the string will be saved*/
 
-				newstring = *format;
+				*newstring = *format; /*put format to newstring to allow being edited*/
 
-					for (i = 0; format[i] != NULL; i++)
+					for (i = 0; format[i] != '\0'; i++)/*check if string has format again to change*/
 					{
-						if (format[i] == '%' && format[i + 1] == 'c')
-						{	*s = newstring;
-							newstring[i] = arg1;
-							while (newstring[i + 1] != NULL)
+						if (format[i] == '%' && format[i + 1] == 'c')/*replaces format with variable*/
+						{	*s = *newstring;
+							newstring[i] = arg;
+							
+							while (newstring[i + 1] != '\0')
 							{
 								newstring [i + 1] = s[i];
 								i++;
 							}
-							va_arg(arg1, char);
 						}
 					}
 			}
 		}
 	va_end(arg1);
+		return (0);
 
 }
