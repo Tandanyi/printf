e89d8e9117252375fd442f78f6eb4378c93fe96f
Tandanyi <tattyndanyi@gmail.com>
2023-03-28 13:12:43 +0000

    added printf.c

--- main.h
+++ main.h
@@ -5,13 +5,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int _printf(const char *format, ....);
-tyedef struct formatter
+int _printf(const char *format, ...);
+
+typedef struct formatter
 {
 	char * symbol;
 	void (*f)(char *);
-}
-void (*converter(char *))(va_list list);
+}f;
+
+void (*converter(const char *))(va_list list);
+
 /******function for specifiers******/
 void print_char(va_list list);
 void print_string(va_list list);
---
+++ printf.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _printf - prints
+ * @format : string literal
+ * return (0);
+ */
+
+int _printf(const char *format, ...)
+{
+	int i;
+	va_list arg;
+
+	va_start(arg, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if(format[i] == '%')
+		{
+			converter(format[++i])(args);
+		}
+		else
+		{
+			write(1, format +i, 1);
+		}
+	}
+	return (0);
+}
